9e333feb3f2218bf97f98d4f0814a4af86d53e6e
Jesús Otero <hello@jesusotero.com>
2026-02-05 00:09:43 +0100

    update explanation of server setup

--- foundation-project/demo/server/index.js
+++ foundation-project/demo/server/index.js
@@ -1,7 +1,17 @@
 import express from "express";
 import knex from "knex";
 
+// Set up express server
 const app = express();
+
+// Support JSON content types in requests
+app.use(express.json());
+
+// Serve frontend files from the app directory
+app.use(express.static("./app"));
+
+
+// Set up database
 const db = knex({
   client: "sqlite3",
   connection: { filename: "./server/database.db" },
@@ -19,4 +29,4 @@ app.get("/leaderboard", async (request, response) => {
   response.json([]);
 });
 
-app.listen(3000, () => console.log("http://localhost:3000"));
+app.listen(3000, () => console.log("App running on http://localhost:3000"));
