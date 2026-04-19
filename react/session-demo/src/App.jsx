075ec4aea668074d0f0cea9cd5e6af76a3faf765
Jesús Otero <hello@jesusotero.com>
2026-04-19 13:38:18 +0200

    refactor to cards into Pet

--- react/session-demo/src/App.jsx
+++ react/session-demo/src/App.jsx
@@ -1,10 +1,46 @@
 import "./App.css";
-import Cat from "./components/Cat";
+import { useState } from "react";
+import Pet from "./components/Pet";
 import Dog from "./components/Dog";
 import Rabbit from "./components/Rabbit";
 import Frog from "./components/Frog";
+import { Section } from "./components/Sections";
 
 function App() {
+  const pets = [
+    {
+      id: 1,
+      name: "Stepan",
+      breed: "Domestic Cat",
+      age: 2,
+    },
+    {
+      id: 2,
+      name: "Not Stepan",
+      breed: "Domestic Cat",
+      age: 2,
+    },
+  ];
+
+  // {
+  //   1: false,
+  //   2: true,
+  //   3: false,
+  // }
+
+  const [petsMarkedAsInterested, setPetsMarkedAsInterested] = useState({});
+
+  const updateInterest = (id, isInterested) => {
+    // In the object of pets interested, mark the one with ID x, as the value isInterested
+    petsMarkedAsInterested[id] = isInterested;
+
+    // We make a copy of the object so react can track it
+    setPetsMarkedAsInterested({ ...petsMarkedAsInterested });
+  };
+
+  const count = Object.values(petsMarkedAsInterested).filter(Boolean).length;
+
+  // Render function: Returned JSX renders.
   return (
     <>
       <header className="app-header">
@@ -15,7 +51,7 @@ function App() {
           <a href="#">Pets</a>
           <a href="#">Contact</a>
         </nav>
-        <span className="favourites-count">♥ Interested in 0 pets</span>
+        <span className="favourites-count">♥ Interested in {count} pets</span>
       </header>
 
       <div className="hero">
@@ -23,15 +59,26 @@ function App() {
         <p>Give a loving home to a pet who needs one</p>
       </div>
 
-      <div className="pets-section">
-        <h2>Available for adoption</h2>
+      <Section title="Available for adoption">
         <div className="card-grid">
-          <Cat />
+          {pets.map((petDetails) => (
+            <Pet
+              key={petDetails.id}
+              details={petDetails}
+              isInterested={petsMarkedAsInterested[petDetails.id]}
+              updateInterest={updateInterest}
+            />
+          ))}
+        </div>
+      </Section>
+
+      <Section title="Recently adopted">
+        <div className="card-grid">
           <Dog />
           <Rabbit />
           <Frog />
         </div>
-      </div>
+      </Section>
     </>
   );
 }
